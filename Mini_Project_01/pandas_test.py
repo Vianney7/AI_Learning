9435999d071c3b99a1003b80192cce9e7daf7aad
Vianney <avianney7@gmail.com>
2026-05-04 08:49:43 -0400

    Lesson 14 - Pandas data analysis

---
+++ Mini_Project_01/pandas_test.py
@@ -0,0 +1,11 @@
+import pandas as pd
+movies = [
+    {'title': 'The Matrix', 'genre': 'Sci-Fi', 'rating': 9.0},
+    {'title': 'The Avengers', 'genre': 'Action', 'rating': 10.0},
+    {'title': 'The Notebook', 'genre': 'Romance', 'rating': 3.5},
+    {'title': 'Inception', 'genre': 'Sci-Fi', 'rating': 8.8},
+    {'title': 'Titanic', 'genre': 'Romance', 'rating': 7.8}]
+
+df = pd.DataFrame(movies)
+print(df)   
+print(df[df['rating']>7.0])
