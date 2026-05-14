fef8dfc91b038ed07c9410c2588968d048ca0490
Vianney <avianney7@gmail.com>
2026-05-14 17:41:26 -0400

    Lesson 16 - AI sentiment analysis on CSV data with Pandas

--- Mini_Project_01/ai_analyser.py
+++ Mini_Project_01/ai_analyser.py
@@ -1,11 +1,30 @@
+sentiments = []
+confidences = []
 import pandas as pd
 from transformers import pipeline
+
 classifier = pipeline('sentiment-analysis') 
+
 def analyze_review(review):
     result = classifier(review)
     label = result[0]['label']
     confidence = round(result[0]['score'] * 100, 1)
     return label, confidence    
+
 print('🎬 AI Movie Review Analyser')
 df = pd.read_csv('movies.csv')
 print(df)
+
+for index, row in df.iterrows():
+    label, confidence = analyze_review(row['review'])
+    sentiments.append(label)
+    confidences.append(confidence)
+
+df['sentiment'] = sentiments
+df['confidence'] = confidences
+
+print('\n🤖 AI Analysis Results:')
+print(df)
+
+df.to_csv('movies_analysed.csv', index=False)
+print('\n✅ Saved to movies_analysed.csv!')
